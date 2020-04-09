966f008687cfa4e6d91ec4ca138ff1e84e7f685f
Simon <stombosa@mum.edu>
2020-04-08 21:31:15 -0500

    draft

--- src/container/Container.js
+++ src/container/Container.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import MenuList from '../components/MenuList/MenuList'
 import MenuDetail from '../components/MenuDetail/MenuDetail'
-// import AddMenu from '../components/AddMenu/AddMenu';
+//import AddMenu from '../components/AddMenu/AddMenu';
 import { Route, Link, Switch, Redirect } from 'react-router-dom'
 import MymenuList from '../components/MyMenuList/MymenuList';
 import NavBar from '../components/Layout/NavBar'
-// import Carousel from '../components/Layout/carousel'
+import Carousel from '../components/Layout/carousel'
 import AsyncComponent from '../hoc/asyncComponent';
 
 const AsyncAddMenu = AsyncComponent(() => {
-    return import('../components/AddMenu/AddMenu')
+    return import('../forms/form')
 })
 
 
@@ -21,36 +21,28 @@ class Container extends Component {
 
     }
 
-    // componentDidMount() {
-    //     axios.get("https://jsonplaceholder.typicode.com/posts?_limit=5").then(res => {
-    //         console.log(res.data);
-    //         const result = res.data
-    //         this.setState({ list: result })
-    //     })
 
-    // }
     render() {
-        // const data = this.state.list.map(item => {
-        //     return <MenuList key={item.id} id={item.userId}  ></MenuList>
-        // })
+
 
         return (
             <div>
                 <NavBar />
                 {/* <Carousel /> */}
-                <header>
+                {/* <header>
                     <nav>
                         <ul>
                             <li><Link to='/'>Home</Link></li>
                             <li><Link to='/add-menu'>Add-Menu</Link></li>
                         </ul>
                     </nav>
-                </header>
-                <Switch>
-                    {this.state.auth ? <Route path='add-menu' exact component={AsyncAddMenu} /> : null}
-                    <Route path='/' exact component={MymenuList} />
-                    <Route path='/:id' exact component={MenuDetail} />
-                </Switch>
+                </header> */}
+                {/* <Switch> */}
+                {this.state.auth ? <Route path='/add-menu' exact component={AsyncAddMenu} /> : null}
+                {/* <Route path='/add-menu' exact component={AsyncAddMenu} /> */}
+                <Route path='/' exact component={MymenuList} />
+                <Route path='/:id' exact component={MenuDetail} />
+                {/* </Switch> */}
             </div >
         )
     }
